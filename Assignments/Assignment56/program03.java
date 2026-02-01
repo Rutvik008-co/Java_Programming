c7490caa2ad70a9c896baf5014fc820ce0e0a81c
Rutvik008-co <rutvikkamble016@gmail.com>
2026-02-01 17:23:50 +0530

    program03:

--- Assignments/Assignment56/program03.java
+++ Assignments/Assignment56/program03.java
@@ -1,28 +1,77 @@
-// Write a program to find the sum of all even numbers up to N
+// Write java program to accept file name from user and open that file in write mode and write some data in this file
 
-class logic 
+import java.io.*;
+import java.util.Scanner;
+
+public class program03
 {
-    void SumEvenNo(int iNo)
+    public static void main(String A[]) throws Exception
     {
-        int iCnt = 0, iSum = 0;
-        for(iCnt = 1; iCnt <= iNo; iCnt++)
+        boolean bRet = false;
+        String FileName = null;
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the name of file");
+        FileName = sobj.nextLine();
+
+        FileWriter fwobj = new FileWriter(FileName);
+        
+        fwobj.write("Jay Ganesh...");
+         
+        fwobj.close();
+        sobj.close();
+    }
+}
+
+/* 
+class program03
+{
+    public static void main(String args[])
+    {
+        try
         {
-            if(iCnt % 2 == 0)
-            {
-                iSum = iSum + iCnt;
-            }
-        }
-        System.out.println(iSum);
-    }
-}
+            Scanner sobj = new Scanner(System.in);
 
-class program01
-{
-    public static void main(String[] args)
-    {
-        
-        logic obj = new logic();
-        obj.SumEvenNo(10);
-        
+            System.out.print("Enter file name : ");
+            String fname = sobj.nextLine();
+
+            // Open / Create file
+            File fobj = new File(fname);
+
+            if (fobj.createNewFile())
+            {
+                System.out.println("File created");
+            }
+            else
+            {
+                System.out.println("File already exists");
+            }
+
+            // Write file
+            FileWriter fw = new FileWriter(fname, true);
+            fw.write("Jay_Ganesh\n");
+
+            fw.close();
+
+            // Read file
+            FileReader fr = new FileReader(fname);
+            BufferedReader br = new BufferedReader(fr);
+
+            String line;
+            System.out.println("\nFile contents:");
+            while ((line = br.readLine()) != null)
+            {
+                System.out.println(line);
+            }
+
+            br.close();
+            sobj.close();
+        }
+        catch (IOException e)
+        {
+            System.out.println("Error: " + e);
+        }
     }
 }
+*/
